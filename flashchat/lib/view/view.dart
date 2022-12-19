0eaf5f9c790376275c386f99330daad9eb86d78c
Apurba Sarker <61203556+Apurba111@users.noreply.github.com>
2022-12-19 15:09:13 +0600

    first commit

---
+++ flashchat/lib/view/view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:61997514957:web:11e220ea4578c7d5022984";
+  static String apiKey = "AIzaSyA6l_b5wpahTJYMK4xxYLLRqn_lHWSly3g";
+  static String messagingSenderId = "61997514957";
+  static String projectId = "chatappafirebase";
+  final primaryColor = const Color(0xFFee7b64);
+}
